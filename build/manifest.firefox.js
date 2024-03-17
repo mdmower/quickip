369d491347969418b2ea4bdb0747a1edb0553349
Matt Mower <mowerm@gmail.com>
2024-03-17 14:19:28 -0700

    Specify min version for Firefox

--- build/manifest.firefox.js
+++ build/manifest.firefox.js
@@ -9,6 +9,7 @@ module.exports = {
   browser_specific_settings: {
     gecko: {
       id: '{56f45803-b8a1-493c-b6e2-d915306e33eb}',
+      strict_min_version: '115.0',
     },
   },
   options_ui: {
