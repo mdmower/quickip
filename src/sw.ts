456048f6863c6e78440468b898969430eb87bfeb
Matt Mower <mowerm@gmail.com>
2024-03-17 13:40:43 -0700

    Remove unnecessary SW event handlers
    The install and activate service worker event handlers are not needed,
    nor are they the preferred means of running installation / startup
    events (see chrome.runtime.onInstalled and chrome.runtime.onStartup).

--- src/sw.ts
+++ src/sw.ts
@@ -3,23 +3,5 @@
  */
 
 import {handleCommand} from './command';
-import {logInfo} from './logger';
-
-self.addEventListener('install', install);
-self.addEventListener('activate', activate);
 
 chrome.commands.onCommand.addListener(handleCommand);
-
-/**
- * Handle service worker install event
- */
-function install() {
-  logInfo('Service worker installed');
-}
-
-/**
- * Handle service worker activate event
- */
-function activate() {
-  logInfo('Service worker activated');
-}
