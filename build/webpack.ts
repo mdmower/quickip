480a9a9a49fda7e31b9deeeecfb633975fc7f82d
Matt Mower <mowerm@gmail.com>
2024-11-22 21:12:05 -0800

    Update packages
    - Release version 7.0
    - Account for updated purgecss import

--- build/webpack.ts
+++ build/webpack.ts
@@ -3,7 +3,7 @@
  */
 
 import path from 'node:path';
-import purgecss from '@fullhuman/postcss-purgecss';
+import {purgeCSSPlugin as purgecss} from '@fullhuman/postcss-purgecss';
 import {Browser, dirRef} from './utils.js';
 import {Configuration} from 'webpack';
 import {EsbuildPlugin} from 'esbuild-loader';
@@ -52,7 +52,7 @@ export function getWebpackConfig(debug: boolean, browser: Browser): Configuratio
                 postcssOptions: {
                   plugins: [
                     // ESM type definitions are incorrect for default purgecss import
-                    (purgecss as unknown as typeof purgecss.default)({
+                    purgecss({
                       contentFunction: (sourceFile) => {
                         const name = path.basename(sourceFile).split('.')[0];
                         const sources = [
