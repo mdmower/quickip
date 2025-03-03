360e109a0088e41ac9309fd0fbceda0c7c3b083e
Matt Mower <mdmower@cmphys.com>
2025-03-02 21:38:42 -0800

    Update packages
    - Use projectService options with typescript-eslint parser

--- eslint.config.js
+++ eslint.config.js
@@ -32,7 +32,7 @@ export default tseslint.config(
     languageOptions: {
       parserOptions: {
         sourceType: 'module',
-        project: true,
+        projectService: true,
       },
     },
     rules: {
